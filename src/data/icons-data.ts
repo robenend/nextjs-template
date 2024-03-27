670c116b7974c96cb26c780cd9c4517ba6ba68cf
robenend <roberaendale2@gmail.com>
2024-03-27 22:09:59 +0300

    init

---
+++ src/data/icons-data.ts
@@ -0,0 +1,690 @@
+export const iconsData = [
+  {
+    "file": "src\\components\\icons\\youtube.tsx",
+    "name": "YoutubeIcon"
+  },
+  {
+    "file": "src\\components\\icons\\xml-solid.tsx",
+    "name": "XMLIcon"
+  },
+  {
+    "file": "src\\components\\icons\\workplace.tsx",
+    "name": "WorkplaceIcon"
+  },
+  {
+    "file": "src\\components\\icons\\weighing-scale.tsx",
+    "name": "WeighingScale"
+  },
+  {
+    "file": "src\\components\\icons\\washing-machine.tsx",
+    "name": "WashingMachineIcon"
+  },
+  {
+    "file": "src\\components\\icons\\walkman.tsx",
+    "name": "WalkmanIcon"
+  },
+  {
+    "file": "src\\components\\icons\\walkman-with-exclamation.tsx",
+    "name": "WalkmanWithExclamationIcon"
+  },
+  {
+    "file": "src\\components\\icons\\visa.tsx",
+    "name": "VisaIcon"
+  },
+  {
+    "file": "src\\components\\icons\\video-solid.tsx",
+    "name": "VideoIcon"
+  },
+  {
+    "file": "src\\components\\icons\\vegetables.tsx",
+    "name": "VegetablesIcon"
+  },
+  {
+    "file": "src\\components\\icons\\users-color.tsx",
+    "name": "UsersColorIcon"
+  },
+  {
+    "file": "src\\components\\icons\\user-settings.tsx",
+    "name": "UserSettingsIcon"
+  },
+  {
+    "file": "src\\components\\icons\\user-plus.tsx",
+    "name": "UserPlusIcon"
+  },
+  {
+    "file": "src\\components\\icons\\user-lock.tsx",
+    "name": "UserLockIcon"
+  },
+  {
+    "file": "src\\components\\icons\\user-info.tsx",
+    "name": "UserInfoIcon"
+  },
+  {
+    "file": "src\\components\\icons\\user-family-color.tsx",
+    "name": "UserFamilyColorIcon"
+  },
+  {
+    "file": "src\\components\\icons\\user-color.tsx",
+    "name": "UserColorIcon"
+  },
+  {
+    "file": "src\\components\\icons\\user-cog.tsx",
+    "name": "UserCog"
+  },
+  {
+    "file": "src\\components\\icons\\user-avatar.tsx",
+    "name": "UserAvatarIcon"
+  },
+  {
+    "file": "src\\components\\icons\\twitter.tsx",
+    "name": "TwitterIcon"
+  },
+  {
+    "file": "src\\components\\icons\\turtle.tsx",
+    "name": "TurtleIcon"
+  },
+  {
+    "file": "src\\components\\icons\\truck.tsx",
+    "name": "TruckIcon"
+  },
+  {
+    "file": "src\\components\\icons\\truck-solid.tsx",
+    "name": "TruckSolidIcon"
+  },
+  {
+    "file": "src\\components\\icons\\trophy.tsx",
+    "name": "TrophyIcon"
+  },
+  {
+    "file": "src\\components\\icons\\trending.tsx",
+    "name": "TrendingIcon"
+  },
+  {
+    "file": "src\\components\\icons\\trending-up.tsx",
+    "name": "TrendingUpIcon"
+  },
+  {
+    "file": "src\\components\\icons\\trending-down.tsx",
+    "name": "TrendingDownIcon"
+  },
+  {
+    "file": "src\\components\\icons\\trash.tsx",
+    "name": "TrashIcon"
+  },
+  {
+    "file": "src\\components\\icons\\transactions.tsx",
+    "name": "TransactionIcon"
+  },
+  {
+    "file": "src\\components\\icons\\ticket.tsx",
+    "name": "TicketIcon"
+  },
+  {
+    "file": "src\\components\\icons\\tent.tsx",
+    "name": "TentIcon"
+  },
+  {
+    "file": "src\\components\\icons\\telegram.tsx",
+    "name": "TelegramIcon"
+  },
+  {
+    "file": "src\\components\\icons\\teams.tsx",
+    "name": "TeamsIcon"
+  },
+  {
+    "file": "src\\components\\icons\\tag.tsx",
+    "name": "TagIcon"
+  },
+  {
+    "file": "src\\components\\icons\\tag-3.tsx",
+    "name": "TagIcon3"
+  },
+  {
+    "file": "src\\components\\icons\\tag-2.tsx",
+    "name": "TagIcon2"
+  },
+  {
+    "file": "src\\components\\icons\\table-sticky-header.tsx",
+    "name": "TableStickyHeaderIcon"
+  },
+  {
+    "file": "src\\components\\icons\\table-search.tsx",
+    "name": "TableSearchIcon"
+  },
+  {
+    "file": "src\\components\\icons\\table-enhanced.tsx",
+    "name": "TableEnhancedIcon"
+  },
+  {
+    "file": "src\\components\\icons\\table-collapsible.tsx",
+    "name": "TableCollapsibleIcon"
+  },
+  {
+    "file": "src\\components\\icons\\table-basic.tsx",
+    "name": "TableBasicIcon"
+  },
+  {
+    "file": "src\\components\\icons\\swimming-pool.tsx",
+    "name": "SwimmingPoolIcon"
+  },
+  {
+    "file": "src\\components\\icons\\sweet.tsx",
+    "name": "SweetIcon"
+  },
+  {
+    "file": "src\\components\\icons\\square-box.tsx",
+    "name": "SquareBoxIcon"
+  },
+  {
+    "file": "src\\components\\icons\\smoke-alarm.tsx",
+    "name": "SmokeAlarmIcon"
+  },
+  {
+    "file": "src\\components\\icons\\slack.tsx",
+    "name": "SlackIcon"
+  },
+  {
+    "file": "src\\components\\icons\\skyscraper.tsx",
+    "name": "SkyscraperIcon"
+  },
+  {
+    "file": "src\\components\\icons\\shopping-cart.tsx",
+    "name": "ShoppingCartIcon"
+  },
+  {
+    "file": "src\\components\\icons\\shopping-cart-alt.tsx",
+    "name": "ShoppingCartAlt"
+  },
+  {
+    "file": "src\\components\\icons\\shopping-bag-solid.tsx",
+    "name": "ShoppingBagSolidIcon"
+  },
+  {
+    "file": "src\\components\\icons\\shop.tsx",
+    "name": "ShopIcon"
+  },
+  {
+    "file": "src\\components\\icons\\shooting-star.tsx",
+    "name": "ShootingStarIcon"
+  },
+  {
+    "file": "src\\components\\icons\\shipping-box.tsx",
+    "name": "ShippingBox"
+  },
+  {
+    "file": "src\\components\\icons\\ship.tsx",
+    "name": "ShipIcon"
+  },
+  {
+    "file": "src\\components\\icons\\ship-with-container.tsx",
+    "name": "ShipWithContainer"
+  },
+  {
+    "file": "src\\components\\icons\\settings-warning.tsx",
+    "name": "SettingsWarningIcon"
+  },
+  {
+    "file": "src\\components\\icons\\sales.tsx",
+    "name": "SalesIcon"
+  },
+  {
+    "file": "src\\components\\icons\\room-single-color.tsx",
+    "name": "RoomSingleIconColor"
+  },
+  {
+    "file": "src\\components\\icons\\room-shared-color.tsx",
+    "name": "RoomSharedIconColor"
+  },
+  {
+    "file": "src\\components\\icons\\rocket-flaming.tsx",
+    "name": "RocketFlamingIcon"
+  },
+  {
+    "file": "src\\components\\icons\\ring-bell-solid.tsx",
+    "name": "RingBellSolidIcon"
+  },
+  {
+    "file": "src\\components\\icons\\revenue-up.tsx",
+    "name": "RevenueUpIcon"
+  },
+  {
+    "file": "src\\components\\icons\\refund.tsx",
+    "name": "RefundIcon"
+  },
+  {
+    "file": "src\\components\\icons\\real-estate.tsx",
+    "name": "RealEstateIcon"
+  },
+  {
+    "file": "src\\components\\icons\\pool-table.tsx",
+    "name": "PoolTableIcon"
+  },
+  {
+    "file": "src\\components\\icons\\point-of-sell.tsx",
+    "name": "PointOfSellIcon"
+  },
+  {
+    "file": "src\\components\\icons\\plane.tsx",
+    "name": "PlaneIcon"
+  },
+  {
+    "file": "src\\components\\icons\\pizza.tsx",
+    "name": "PizzaIcon"
+  },
+  {
+    "file": "src\\components\\icons\\pine-apple.tsx",
+    "name": "PineAppleIcon"
+  },
+  {
+    "file": "src\\components\\icons\\pie-chart-currency.tsx",
+    "name": "PieChartCurrencyIcon"
+  },
+  {
+    "file": "src\\components\\icons\\pencil.tsx",
+    "name": "PencilIcon"
+  },
+  {
+    "file": "src\\components\\icons\\pdf-solid.tsx",
+    "name": "PDFIcon"
+  },
+  {
+    "file": "src\\components\\icons\\pasta.tsx",
+    "name": "PastaIcon"
+  },
+  {
+    "file": "src\\components\\icons\\parcel-map.tsx",
+    "name": "ParcelMapIcon"
+  },
+  {
+    "file": "src\\components\\icons\\page-blank.tsx",
+    "name": "PageBlankIcon"
+  },
+  {
+    "file": "src\\components\\icons\\package-open.tsx",
+    "name": "PackageOpenIcon"
+  },
+  {
+    "file": "src\\components\\icons\\online-payment.tsx",
+    "name": "OnlinePaymentIcon"
+  },
+  {
+    "file": "src\\components\\icons\\notion.tsx",
+    "name": "NotionIcon"
+  },
+  {
+    "file": "src\\components\\icons\\notification-settings.tsx",
+    "name": "NotificationSettingsIcon"
+  },
+  {
+    "file": "src\\components\\icons\\nft.tsx",
+    "name": "NftIcon"
+  },
+  {
+    "file": "src\\components\\icons\\newsletter-announcement.tsx",
+    "name": "NewsletterAnnouncement"
+  },
+  {
+    "file": "src\\components\\icons\\music-solid.tsx",
+    "name": "MusicIcon"
+  },
+  {
+    "file": "src\\components\\icons\\multi-step-arrow.tsx",
+    "name": "MultiStepArrowIcon"
+  },
+  {
+    "file": "src\\components\\icons\\money-in-hand.tsx",
+    "name": "MoneyInHand"
+  },
+  {
+    "file": "src\\components\\icons\\mobile-home.tsx",
+    "name": "MobileHomeIcon"
+  },
+  {
+    "file": "src\\components\\icons\\meter.tsx",
+    "name": "MeterIcon"
+  },
+  {
+    "file": "src\\components\\icons\\medium.tsx",
+    "name": "MediumIcon"
+  },
+  {
+    "file": "src\\components\\icons\\mastercard.tsx",
+    "name": "MasterCardIcon"
+  },
+  {
+    "file": "src\\components\\icons\\map-marker-with-path.tsx",
+    "name": "MapMarkerWithPathIcon"
+  },
+  {
+    "file": "src\\components\\icons\\magnifying-with-cross.tsx",
+    "name": "MagnifyingWithCrossIcon"
+  },
+  {
+    "file": "src\\components\\icons\\magnifying-with-bar.tsx",
+    "name": "MagnifyingWithBarIcon"
+  },
+  {
+    "file": "src\\components\\icons\\magnifying-glass-color.tsx",
+    "name": "MagnifyingGlassIconColor"
+  },
+  {
+    "file": "src\\components\\icons\\lunch.tsx",
+    "name": "LunchIcon"
+  },
+  {
+    "file": "src\\components\\icons\\luggage.tsx",
+    "name": "LuggageIcon"
+  },
+  {
+    "file": "src\\components\\icons\\luggage-two.tsx",
+    "name": "LuggageTwoIcon"
+  },
+  {
+    "file": "src\\components\\icons\\lock-wild-card.tsx",
+    "name": "LockWildCardIcon"
+  },
+  {
+    "file": "src\\components\\icons\\lock-exclamation.tsx",
+    "name": "LockExclamationIcon"
+  },
+  {
+    "file": "src\\components\\icons\\linkedin.tsx",
+    "name": "LinkedinIcon"
+  },
+  {
+    "file": "src\\components\\icons\\light-mode.tsx",
+    "name": "LightMode"
+  },
+  {
+    "file": "src\\components\\icons\\kitchen.tsx",
+    "name": "KitchenIcon"
+  },
+  {
+    "file": "src\\components\\icons\\invoice.tsx",
+    "name": "InvoiceIcon"
+  },
+  {
+    "file": "src\\components\\icons\\invoice-print.tsx",
+    "name": "InvoicePrint"
+  },
+  {
+    "file": "src\\components\\icons\\instagram.tsx",
+    "name": "InstagramIcon"
+  },
+  {
+    "file": "src\\components\\icons\\image-solid.tsx",
+    "name": "ImageIcon"
+  },
+  {
+    "file": "src\\components\\icons\\house.tsx",
+    "name": "HouseIcon"
+  },
+  {
+    "file": "src\\components\\icons\\house-boat.tsx",
+    "name": "HouseBoatIcon"
+  },
+  {
+    "file": "src\\components\\icons\\hour-glass.tsx",
+    "name": "HourGlassIcon"
+  },
+  {
+    "file": "src\\components\\icons\\hand-wave.tsx",
+    "name": "HandWaveIcon"
+  },
+  {
+    "file": "src\\components\\icons\\half-circle-right.tsx",
+    "name": "HalfCircleRightIcon"
+  },
+  {
+    "file": "src\\components\\icons\\half-circle-left.tsx",
+    "name": "HalfCircleLeftIcon"
+  },
+  {
+    "file": "src\\components\\icons\\gym.tsx",
+    "name": "GymIcon"
+  },
+  {
+    "file": "src\\components\\icons\\green-leaf.tsx",
+    "name": "GreenLeafIcon"
+  },
+  {
+    "file": "src\\components\\icons\\google.tsx",
+    "name": "GoogleIcon"
+  },
+  {
+    "file": "src\\components\\icons\\github.tsx",
+    "name": "GithubIcon"
+  },
+  {
+    "file": "src\\components\\icons\\french-fries.tsx",
+    "name": "FrenchFriesIcon"
+  },
+  {
+    "file": "src\\components\\icons\\folder-solid.tsx",
+    "name": "FolderIcon"
+  },
+  {
+    "file": "src\\components\\icons\\folder-lock.tsx",
+    "name": "FolderLockIcon"
+  },
+  {
+    "file": "src\\components\\icons\\flight-icon.tsx",
+    "name": "FlightIcon"
+  },
+  {
+    "file": "src\\components\\icons\\fire-extinguisher.tsx",
+    "name": "FireExtinguisherIcon"
+  },
+  {
+    "file": "src\\components\\icons\\financial-statistics.tsx",
+    "name": "FinancialStatisticsIcon"
+  },
+  {
+    "file": "src\\components\\icons\\files.tsx",
+    "name": "FilesIcon"
+  },
+  {
+    "file": "src\\components\\icons\\file-stack.tsx",
+    "name": "FileStackIcon"
+  },
+  {
+    "file": "src\\components\\icons\\file-settings.tsx",
+    "name": "FileSettingsIcon"
+  },
+  {
+    "file": "src\\components\\icons\\figma.tsx",
+    "name": "FigmaIcon"
+  },
+  {
+    "file": "src\\components\\icons\\farm-house.tsx",
+    "name": "FarmHouseIcon"
+  },
+  {
+    "file": "src\\components\\icons\\facebook.tsx",
+    "name": "FacebookIcon"
+  },
+  {
+    "file": "src\\components\\icons\\eye.tsx",
+    "name": "EyeIcon"
+  },
+  {
+    "file": "src\\components\\icons\\express-delivery.tsx",
+    "name": "ExpressDeliveryIcon"
+  },
+  {
+    "file": "src\\components\\icons\\expenses.tsx",
+    "name": "ExpenseIcon"
+  },
+  {
+    "file": "src\\components\\icons\\exchange.tsx",
+    "name": "ExchangeIcon"
+  },
+  {
+    "file": "src\\components\\icons\\envato.tsx",
+    "name": "EnvatoIcon"
+  },
+  {
+    "file": "src\\components\\icons\\drive-solid.tsx",
+    "name": "DriveIcon"
+  },
+  {
+    "file": "src\\components\\icons\\doc-solid.tsx",
+    "name": "DocIcon"
+  },
+  {
+    "file": "src\\components\\icons\\dinner.tsx",
+    "name": "DinnerIcon"
+  },
+  {
+    "file": "src\\components\\icons\\dining.tsx",
+    "name": "DiningIcon"
+  },
+  {
+    "file": "src\\components\\icons\\dices.tsx",
+    "name": "DicesIcon"
+  },
+  {
+    "file": "src\\components\\icons\\delivery-truck.tsx",
+    "name": "DeliveryTruckIcon"
+  },
+  {
+    "file": "src\\components\\icons\\dark-mode.tsx",
+    "name": "DarkMode"
+  },
+  {
+    "file": "src\\components\\icons\\cube-solid.tsx",
+    "name": "CubeSolidIcon"
+  },
+  {
+    "file": "src\\components\\icons\\cookies.tsx",
+    "name": "CookiesIcon"
+  },
+  {
+    "file": "src\\components\\icons\\containers.tsx",
+    "name": "Containers"
+  },
+  {
+    "file": "src\\components\\icons\\container-house.tsx",
+    "name": "ContainerHouseIcon"
+  },
+  {
+    "file": "src\\components\\icons\\cold-drink.tsx",
+    "name": "ColdDrinkIcon"
+  },
+  {
+    "file": "src\\components\\icons\\cog-solid.tsx",
+    "name": "CogSolidIcon"
+  },
+  {
+    "file": "src\\components\\icons\\coffee-cup.tsx",
+    "name": "CoffeeCupIcon"
+  },
+  {
+    "file": "src\\components\\icons\\cloud-task.tsx",
+    "name": "CloudTaskIcon"
+  },
+  {
+    "file": "src\\components\\icons\\chicken.tsx",
+    "name": "ChickenIcon"
+  },
+  {
+    "file": "src\\components\\icons\\chat-solid.tsx",
+    "name": "ChatSolidIcon"
+  },
+  {
+    "file": "src\\components\\icons\\CCCamera.tsx",
+    "name": "CCCameraIcon"
+  },
+  {
+    "file": "src\\components\\icons\\cave.tsx",
+    "name": "CaveIcon"
+  },
+  {
+    "file": "src\\components\\icons\\castle.tsx",
+    "name": "CastleIcon"
+  },
+  {
+    "file": "src\\components\\icons\\cargo.tsx",
+    "name": "Cargo"
+  },
+  {
+    "file": "src\\components\\icons\\cargo-pallet.tsx",
+    "name": "CargoPallet"
+  },
+  {
+    "file": "src\\components\\icons\\cargo-damaged.tsx",
+    "name": "CargoDamaged"
+  },
+  {
+    "file": "src\\components\\icons\\car-parking.tsx",
+    "name": "CarParkingIcon"
+  },
+  {
+    "file": "src\\components\\icons\\calendar.tsx",
+    "name": "CalenderIcon"
+  },
+  {
+    "file": "src\\components\\icons\\cabin.tsx",
+    "name": "CabinIcon"
+  },
+  {
+    "file": "src\\components\\icons\\business.tsx",
+    "name": "BusinessIcon"
+  },
+  {
+    "file": "src\\components\\icons\\burger.tsx",
+    "name": "BurgerIcon"
+  },
+  {
+    "file": "src\\components\\icons\\bulb-solid.tsx",
+    "name": "BulbSolidIcon"
+  },
+  {
+    "file": "src\\components\\icons\\brush-solid.tsx",
+    "name": "BrushSolidIcon"
+  },
+  {
+    "file": "src\\components\\icons\\beef.tsx",
+    "name": "BeefIcon"
+  },
+  {
+    "file": "src\\components\\icons\\bbq-grill.tsx",
+    "name": "BBQGrillIcon"
+  },
+  {
+    "file": "src\\components\\icons\\barn.tsx",
+    "name": "BarnIcon"
+  },
+  {
+    "file": "src\\components\\icons\\bag.tsx",
+    "name": "BagIcon"
+  },
+  {
+    "file": "src\\components\\icons\\badge-not-allowed.tsx",
+    "name": "BadgeNotAllowedIcon"
+  },
+  {
+    "file": "src\\components\\icons\\arrow-both-direction.tsx",
+    "name": "ArrowBothDirectionIcon"
+  },
+  {
+    "file": "src\\components\\icons\\apple-pay.tsx",
+    "name": "ApplePayIcon"
+  },
+  {
+    "file": "src\\components\\icons\\apartment.tsx",
+    "name": "ApartmentIcon"
+  },
+  {
+    "file": "src\\components\\icons\\analytics-circular.tsx",
+    "name": "AnalyticsCircularIcon"
+  },
+  {
+    "file": "src\\components\\icons\\airtable.tsx",
+    "name": "AirtableIcon"
+  },
+  {
+    "file": "src\\components\\icons\\air-conditioner.tsx",
+    "name": "AirConditionerIcon"
+  }
+];
